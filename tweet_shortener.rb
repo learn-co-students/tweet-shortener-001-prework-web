6d3f9fb621498d52bbdaeedb30b7a2dfc33a1b2e
shmulim <shmuel.markel@gmail.com>
2015-11-23 12:58:36 -0800

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split.map { |wrd| dictionary.has_key?(wrd) ? wrd = dictionary[wrd] : wrd }.join(" ")
+end
