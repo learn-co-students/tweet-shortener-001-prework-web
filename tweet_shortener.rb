4f56077f29aa1bc7f57a71634e16a09455259766
jd2rogers2 <jd2rogers2@gmail.com>
2016-02-20 13:01:55 -0500

    donzo

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,54 @@
 # Write your code here.
+
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "For" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(string)
+  array = string.split(" ")
+  new_array = []
+  array.each do |x|
+    if dictionary.keys.include?(x) == true
+      new_array << dictionary.values_at(x)
+    else
+      new_array << x
+    end
+  end
+  new_array.join(" ")
+end
+
+def bulk_tweet_shortener(array_of_strings)
+  array_of_strings.each do |x|
+    puts word_substituter(x)
+  end
+end
+
+def selective_tweet_shortener(string)
+  if string.length > 140
+    word_substituter(string)
+  else
+    string
+  end
+end
+
+def shortened_tweet_truncator(string)
+  if string.length > 140
+    new_string = string[0..139]
+  else
+    new_string = string
+  end
+  new_string
+end
+
