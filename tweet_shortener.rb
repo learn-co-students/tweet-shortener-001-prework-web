aedca1a329c517914ff56fda64963fd26d2fa08c
hdwatts <howard@hdwatts.com>
2016-03-02 12:06:18 -0500

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,51 @@
 # Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  words = tweet.split(" ")
+  dict = dictionary()
+  shortened_words = words.collect do |word|
+    dword = word.downcase
+    if dict[dword] != nil
+      word = dict[dword]
+    else
+      word
+    end
+  end
+  shortened_words.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet = selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    tweet = tweet[1..140]
+  end
+  tweet
+end
