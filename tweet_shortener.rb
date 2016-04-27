c67602e717efeb58659ab939d1a21121f367380e
supermancbs <clark.sacktor@gmail.com>
2016-04-26 22:32:07 -0400

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,70 +1,46 @@
 # Write your code here.
 
 def dictionary
-	{"hello" => 'hi',
-	"to, two, too" => '2', 
-	"for, four" => '4',
-	'be' => 'b',
-	'you' => 'u',
-	"at" => "@",
-	"and" => "&"}
-end
+dictionary = {
+  "too" => "2",
+  "to" => "2",
+  "two" =>"2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end 
 
+def word_substituter(tweet)
+	tweet.split(" ").collect do |i|
+		if dictionary.include?(i.downcase)
+			i=dictionary[i.downcase]
+		else 
+			i
+		end 
+	end.join(" ")
 
- 
-def word_substituter(string)
-	
-	new = string.split(" ")
-	
-	dictionary.each do |person, data|
- 		
- 	 		ind=0
-			while ind < new.length 
-				
-				if person.downcase.include?(new[ind].downcase) && new[ind].downcase != "a"
-					new[ind]=data
-				end 
-				ind+=1
-			end 
-		
-		 
-	end  
-	  
-	   new.join(" ")
 end 
 
 def bulk_tweet_shortener(array)
 	array.each do |i|
 		puts word_substituter(i)
-		
 	end 
-	
-
 end 
 
-
-def selective_tweet_shortener(string)
-	if string.length > 140
-		return word_substituter(string)
-	
-	else 
-	
-		return string
-	end 
-
-
+def selective_tweet_shortener(tweet)
+	if tweet.length > 140
+		return word_substituter(tweet)
+	end
+	tweet
 end 
 
-def shortened_tweet_truncator(string)
-	new = word_substituter(string)
-	if new.length > 140
-	
-		return new[0..134] + "(...)"
-	else 
-		return new 
+def shortened_tweet_truncator(tweet)
+	if word_substituter(tweet).length > 140
+		return word_substituter(tweet)[0..136] + "..."
 	end 
-
+	tweet 
 end 
-
-
-
