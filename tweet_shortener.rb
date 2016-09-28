4b47d0aae29608f61e4d7c4b342b1faf4f7a68b2
Rachel Salois <rachel.salois@gmail.com>
2016-09-28 12:15:15 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+def dictionary
+{
+"hello" => 'hi',
+"to" => '2',
+"two" => '2',
+"too" => '2',
+"for" => '4',
+"four" => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@",
+"and" => "&"
+}
+end
+
+
+def word_substituter(tweet)
+  arr = tweet.split(" ")
+  words = dictionary.keys
+  replacements = dictionary.values
+  a_index = 0
+while a_index < arr.length
+  	w_index = 0
+  	while w_index < words.length
+  		if arr[a_index] == words[w_index]
+  			arr.delete_at(a_index)
+  			arr.insert(a_index, replacements[w_index])
+  		end
+  		w_index+=1
+  	end
+  	a_index+=1
+  end
+  	return arr.join(" ")
+
+end
